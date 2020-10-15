f115d343b97aa3c2e2b75d6e1bcb7efad6379bba
mrom <mrom@posteo.de>
2020-10-16 01:39:27 +0200

    Designate get methods as private and introduce fill method

--- music_collection.py
+++ music_collection.py
@@ -1,6 +1,5 @@
 """Provides the MusicCollection class which represents a music
-collection and includes statistical methods for analyzing the
-collection.
+collection and includes statistical methods for analyzing it.
 """
 
 import os, re
@@ -10,26 +9,26 @@ class MusicCollection:
 	def __init__(self, music_collection_path):
 		self.music_collection_path = music_collection_path
 		self.paths = []
-		self.list_of_years = []
+		self.years = []
 
-	def get_all_paths(self):
+	def _get_paths(self):
 		for path, _, _ in os.walk(self.music_collection_path):
 			self.paths.append(path)
 
-	def get_list_of_years(self):
-		r = re.compile(r'([1-2][0-9]{3}).*$')  # .../.../1994 ...
-		matches = [r.findall(path) for path in self.paths]  # [None, ['1994'], ...]
-		self.list_of_years = sorted(  # [1994, ...]
+	def _get_years(self):
+		contains_year = re.compile(r'([1-2][0-9]{3}).*$')  # .../.../(1994) ...
+		matches = [contains_year.findall(path) for path in self.paths]  # [None, ['1994'], ...]
+		self.years = sorted(  # [1994, ...]
 				[int(match[0]) for match in matches if match]
 			)
 
 	def fill(self):
-		self.get_all_paths()
-		self.get_list_of_years()
+		self._get_paths()
+		self._get_years()
 
 	def plot_histo(self):
-		zeitraum = max(self.list_of_years) - min(self.list_of_years)
-		plt.hist(self.list_of_years, zeitraum+1)
+		zeitraum = max(self.years) - min(self.years)
+		plt.hist(self.years, zeitraum+1)
 		plt.xlabel('Jahr')
 		plt.ylabel('Anzahl Alben')
 		plt.title('Anzahl Alben pro Jahr')
